b02f6166ddaaee2d9ecb50921a7ae4a645b805f3
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2026-08-24 13:27:57 +0300

    cmd/: Fixed dbd locations

--- cmd/q2.cmd
+++ cmd/q2.cmd
@@ -1,3 +1,8 @@
+#!../../bin/linux-x86_64/ioc
+
+dbLoadDatabase "dbd/ioc.dbd"
+ioc_registerRecordDeviceDriver pdbbase
+
 dbLoadRecords("db/optics-q2.db")
 
 PSCConfigure("C05-QF1", "10.2.2.89:9322")
@@ -19,4 +24,6 @@ PSCConfigure("C08-QF1", "10.2.2.101:9322")
 PSCConfigure("C08-QD1", "10.2.2.102:9322")
 PSCConfigure("C08-QF2", "10.2.2.103:9322")
 PSCConfigure("C08-QD2", "10.2.2.104:9322")
+
+iocInit
 
--- cmd/st.cmd
+++ cmd/st.cmd
@@ -1,9 +1,13 @@
-#!bin/linux-x86_64/ioc
+#!../../bin/linux-x86_64/ioc
 
 dbLoadDatabase "dbd/ioc.dbd"
 ioc_registerRecordDeviceDriver pdbbase
 
-< cmd/q1.cmd
+dbLoadRecords("db/correctors.db")
+dbLoadRecords("db/optics.db")
+
+drvAsynIPPortConfigure("PS_QF1_1", "", 0, 0, 0)
+PSCConfigure("QF1_1", "PS_QF1_1")
 
 iocInit
 
